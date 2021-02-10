9b0e8ea1c698fcaab94756eca1c0a787eea39e1b
mthanki <60077334+mthanki@users.noreply.github.com>
2021-02-10 18:48:07 -0500

    Add project files.

---
+++ Tip, Tax and Total/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tip__Tax_and_Total
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        const decimal TIP_PERCENT = 15;
+        const decimal SALES_TAX_PERCENT = 7;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ApplyErrorStyle()
+        {
+            BillAmountTextBox.BorderBrush = Brushes.Red;
+        }
+
+        private void ResetToMainStyle()
+        {
+            BillAmountTextBox.BorderBrush = new SolidColorBrush(Color.FromArgb(100, 171, 173, 179));
+        }
+
+        private void CalculateTotalAmount_Click(object sender, RoutedEventArgs e)
+        {
+            ResetToMainStyle();
+            try
+            {
+                decimal BillAmount = decimal.Parse(BillAmountTextBox.Text);
+
+                decimal TipAmount = (BillAmount * TIP_PERCENT) / 100;
+                decimal SalesTaxAmount = (BillAmount * SALES_TAX_PERCENT) / 100;
+
+                decimal TotalAmount = BillAmount + TipAmount + SalesTaxAmount;
+
+                TipAmountLabel.Content = TipAmount;
+                SalesTaxAmountLabel.Content = SalesTaxAmount;
+                TotalAmountLabel.Content = TotalAmount;
+
+            }
+            catch
+            {
+                BillAmountTextBox.Text = "";
+                ApplyErrorStyle();
+                ErrorWindow E = new ErrorWindow();
+                E.Show();
+            }
+        }
+    }
+}
